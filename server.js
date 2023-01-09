16d7c5831cf7118ab9208cadf1c06f35ac0ab3f4
1ilsang <1ilsang@naver.com>
2023-01-09 19:19:31 +0900

    feat(server): Add random ms

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const cors = require("@koa/cors");
 
 const app = new Koa();
 
-const sleep = (ms = 1200) => new Promise((resolve) => setTimeout(resolve, ms));
+// 500 ~ 1500 ms
+const getRandom = Math.floor(Math.random() * 1000 + 500);
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 app.use(cors());
 
@@ -12,15 +15,15 @@ const list = [Number(new Date())];
 // response
 app.use(async (ctx) => {
   if (ctx.path === "/") {
-    await sleep();
+    await sleep(getRandom());
     ctx.body = { text: "Hello, World!" };
     return;
   } else if (ctx.path === "/list") {
-    await sleep();
+    await sleep(getRandom());
     ctx.body = { text: "success", list };
     return;
   } else if (ctx.path === "/add") {
-    await sleep();
+    await sleep(getRandom());
     list.push(Number(new Date()));
     ctx.body = { text: "success", list };
   }
